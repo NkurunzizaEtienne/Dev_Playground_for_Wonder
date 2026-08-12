feab739cdfdcc95926bde5e9ecf33b1815adac2f
NKURUNZIZA Etienne <enkurunziza2002@gmail.com>
2026-08-12 17:09:45 +0200

    just made IdPipe injectable so It can be used even beyond property module

--- backend_playground/NestJs/basics/src/property/CustomPipe/IdPipe.ts
+++ backend_playground/NestJs/basics/src/property/CustomPipe/IdPipe.ts
@@ -1,6 +1,7 @@
-import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
 import { isPositive } from "class-validator";
 
+@Injectable()
 export class ParseIdPipe implements PipeTransform<string, number> {
     transform(value: string, metadata: ArgumentMetadata): number {
         const val = parseInt(value, 10) // using the base 10
